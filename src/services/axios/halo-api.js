d784795e7e282a6b04d996627dbb1dd26cb6656a
Adri Blanco <adriblabla@gmail.com>
2018-07-04 20:47:37 +0200

    Updated the function to retrieve the HALO token

--- src/services/axios/halo-api.js
+++ src/services/axios/halo-api.js
@@ -1,9 +1,27 @@
 import axios from 'axios';
+import pathnames from '../../pathnames';
 
-export default axios.create({
+let token;
+let expirationDate;
+
+export const getAuthToken = async () => {
+  if(!token || new Date() > expirationDate) {
+    const res = await AxiosHALO.post(pathnames.getToken(), {
+      username: process.env.HALO_USERNAME,
+      password: process.env.HALO_PASSWORD,
+      grant_type: 'password',
+    });
+
+    token= res.data.access_token,
+    expirationDate = new Date().getTime() + res.data.expires_in
+  }
+  return token;
+}
+
+export const AxiosHALO = axios.create({
   baseURL: 'https://halo-stage.mobgen.com/api/',
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   }
 });
