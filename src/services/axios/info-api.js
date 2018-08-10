e1912b80f35a0d598df8d085a57321e6fa146e54
Adri Blanco <adriblabla@gmail.com>
2018-08-10 16:23:36 +0200

    Added new interceptor and handlers to the info api

--- src/services/axios/info-api.js
+++ src/services/axios/info-api.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
+import pathnames from '../../pathnames';
 
-export default axios.create({
-  baseURL: 'http://api.football-data.org/v1/',
+const InfoAxios =  axios.create({
+  baseURL: process.env.INFO_BASE_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'X-Auth-Token': process.env.InfoAuthToken
+    'X-Auth-Token': process.env.INFO_AUTH_TOKEN
   }
 });
+
+InfoAxios.interceptors.response.use(
+  function (res) {
+    return Promise.resolve(res.data);
+  },
+  function (err) {
+    return Promise.reject(err.data);
+  }
+);
+
+export const getLeagueInfo = (leagueId) => {
+  return InfoAxios.get(pathnames.getLeague(leagueId));
+};
